d52b57e1d29b685331063c34858d8f440b1e5555
Marc Rousavy <marcrousavy@hotmail.com>
2020-01-18 16:14:00 +0100

    use own pkg

--- views/StartScreen.js
+++ views/StartScreen.js
@@ -1,7 +1,6 @@
-import React, {Component} from 'react';
-import { Modal, Button, StyleSheet, Text, View, TouchableHighlight, NativeModules, Alert } from 'react-native';
-import CardStack, { Card } from 'react-native-card-stack-swiper';
-import { ImageBrowser } from 'expo-multiple-media-imagepicker';
+import React from 'react';
+import { Modal, StyleSheet, Text, View, TouchableHighlight, Alert } from 'react-native';
+import { ImageBrowser } from '../lib/ImageBrowser';
 import * as Permissions from 'expo-permissions';
 
 
@@ -25,19 +24,12 @@ export class StartScreen extends React.Component {
                     onRequestClose={() => {
                         Alert.alert('Modal has been closed.');
                     }}>
-                    <View style={{marginTop: 22}}>
-                        <Text>Pick an image</Text>
-                        <ImageBrowser
-                            callback={(num, onSubmit) => {}}
-                            headerCloseText={'キャンセル'} // Close button text on header. default is 'Close'.
-                            headerDoneText={'　　完了'} // Done button text on header. default is 'Done'.
-                            headerButtonColor={'#E31676'} // Button color on header.
-                            headerSelectText={'枚の画像を選択中'} // Word when picking.  default is 'n selected'.
-                            mediaSubtype={'screenshot'} // Only iOS, Filter by MediaSubtype. default is display all.
-                            badgeColor={'#E31676'} // Badge color when picking.
-                            emptyText={'選択できる画像がありません'} // Empty Text
-                            />
-                    </View>
+                        <View>
+                            <ImageBrowser
+                                onChange={(num) => {console.log(num);}}
+                                callback={(num, onSubmit) => {}}
+                                />
+                        </View>
                 </Modal>
 
                 <TouchableHighlight
