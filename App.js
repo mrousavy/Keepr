e83e832489c35a6ca0f8dd612d7aeebe71137ed9
Marc Rousavy <marcrousavy@hotmail.com>
2020-01-18 12:18:25 +0100

    add imagepicker

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, NativeModules } from 'react-native';
 import CardStack, { Card } from 'react-native-card-stack-swiper';
+
+var ImagePicker = NativeModules.ImageCropPicker;
+
+function pickImages() {
+  ImagePicker.openPicker({
+    multiple: true
+  }).then(images => {
+    console.log(images);
+  });
+}
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={() => pickImages()} style={styles.button}>
+        <Text style={styles.text}>Select Single Image With Camera</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +31,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: 'blue',
+    marginBottom: 10
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center'
+  }
 });
