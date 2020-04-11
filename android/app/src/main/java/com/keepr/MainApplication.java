fe078bfee7f503bb8296920e4c2777f535674cc0
Christoph Pader <chrispad2k@gmail.com>
2020-04-11 14:18:33 +0200

    Added react-native-cameraroll

--- android/app/src/main/java/com/keepr/MainApplication.java
+++ android/app/src/main/java/com/keepr/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.cameraroll.CameraRollPackage;
 import com.mkuczera.RNReactNativeHapticFeedbackPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
