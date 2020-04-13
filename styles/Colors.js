fe6b45c39d6965ce03d43a4f7d75135707fea97e
Christoph Pader <chrispad2k@gmail.com>
2020-04-13 17:23:07 +0200

    Create Colors.js

---
+++ styles/Colors.js
@@ -0,0 +1,13 @@
+const Colors = {
+  bg: '#FFFFFF',
+  bgDark: '#F6F8FA',
+  bgDarker: '#EEEDE8',
+
+  primary: '#0C82E8',
+  primaryDark: '#0089EB',
+  primaryDarker: '#0D76FF',
+  primaryLight: '#00CFEB',
+  primarLighter: '#00FFF6',
+};
+
+export default Colors;
