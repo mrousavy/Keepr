2ad2c74e5e9700344e7e80080ad6640f937c76bf
Christoph Pader <chrispad2k@gmail.com>
2020-04-13 21:30:00 +0200

    Update Colors.js

--- styles/Colors.js
+++ styles/Colors.js
@@ -3,6 +3,10 @@ const Colors = {
   bgDark: '#F6F8FA',
   bgDarker: '#EEEDE8',
 
+  text: '#000000',
+  textLight: '#D3D3D3',
+  textLighter: '#DDDDDD',
+
   primary: '#0C82E8',
   primaryDark: '#0089EB',
   primaryDarker: '#0D76FF',
