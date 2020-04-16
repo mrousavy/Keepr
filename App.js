58f0bba9f6c08ec3c3467a1598e5a6bf28e5493c
Christoph Pader <chrispad2k@gmail.com>
2020-04-16 11:27:44 +0200

    Rearranged structure for Shared Element Navigation

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
-import {HomeScreen} from './views/HomeScreen';
-import {SwipeScreen} from './views/SwipeScreen';
+import {Home} from './views/home/Home';
+import {Swipe} from './views/swipe/Swipe';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 Icon.loadFont();
 
-const MainNavigator = createStackNavigator();
+const Stack = createStackNavigator();
 
-const App = () => {
-  return (
-    <NavigationContainer>
-      <MainNavigator.Navigator initialRouteName="Home">
-        <MainNavigator.Screen name="Home" component={HomeScreen} />
-        <MainNavigator.Screen name="Swipe" component={SwipeScreen} />
-      </MainNavigator.Navigator>
-    </NavigationContainer>
-  );
-};
-
-export default App;
+export default class App extends React.PureComponent {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{gestureEnabled: false}}
+          headerMode="none"
+          initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Swipe" component={Swipe} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
---
+++ views/home/Home.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import {TransitionPresets} from '@react-navigation/stack';
+import {createSharedElementStackNavigator} from 'react-navigation-shared-element';
+import {HomeScreen} from './HomeScreen';
+import {CollectionDetailScreen} from './CollectionDetailScreen';
+
+export const iosTransitionSpec = {
+  animation: 'spring',
+  config: {
+    stiffness: 1000,
+    damping: 500,
+    mass: 3,
+    overshootClamping: true,
+    restDisplacementThreshold: 10,
+    restSpeedThreshold: 10,
+  },
+};
+
+const SharedElementStack = createSharedElementStackNavigator();
+export const Home = () => {
+  return (
+    <SharedElementStack.Navigator
+      mode="modal"
+      screenOptions={{
+        useNativeDriver: true,
+        gestureEnabled: false,
+        // gestureResponseDistance: {
+        // 	vertical: 100,
+        // },
+        // gestureDirection: 'vertical',
+
+        // cardOverlayEnabled: true,
+        ...TransitionPresets.ModalSlideFromBottomIOS,
+        transitionSpec: {
+          open: iosTransitionSpec,
+          close: iosTransitionSpec,
+        },
+        cardStyleInterpolator: ({current: {progress}}) => ({
+          cardStyle: {
+            opacity: progress,
+          },
+        }),
+      }}
+      headerMode="none">
+      <SharedElementStack.Screen name="HomeScreen" component={HomeScreen} />
+      <SharedElementStack.Screen
+        name="CollectionDetailScreen"
+        component={CollectionDetailScreen}
+        sharedElementsConfig={(route, otherRoute, showing) => {
+          const {item} = route.params;
+          if (route.name === 'ItemDetailsScreen' && showing) {
+            return [
+              {
+                id: `item.${item.id}.image`,
+              },
+              {
+                id: `item.${item.id}.title`,
+                animation: 'fade',
+                resize: 'clip',
+                align: 'left-top',
+              },
+              {
+                id: `item.${item.id}.description`,
+                animation: 'fade',
+                resize: 'clip',
+                align: 'left-top',
+              },
+            ];
+          } else {
+            return [
+              {
+                id: `item.${item.id}.image`,
+              },
+            ];
+          }
+        }}
+      />
+    </SharedElementStack.Navigator>
+  );
+};
