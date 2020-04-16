9c552231de4db4a50a243f4c4cb7b76589f0b9bb
Christoph Pader <chrispad2k@gmail.com>
2020-04-16 11:27:12 +0200

    Update PhotoLibrary.js

--- components/PhotoLibrary.js
+++ components/PhotoLibrary.js
@@ -1,44 +1,44 @@
 import CameraRoll from '@react-native-community/cameraroll';
 
-class PhotoLibrary {
-  constructor() {
-    this.photos = {};
-    this.albums = {};
-    this.collections = {};
-  }
+export let photos = {};
+export let albums = {};
+export let collections = {};
 
-  async loadPhotos(count = 100) {
-    let photos = await CameraRoll.getPhotos({first: count});
-    this.photos = photos.edges;
-  }
+export let loadPhotos = async (count = 100) => {
+  let cameraRoll = await CameraRoll.getPhotos({first: count});
+  photos = cameraRoll.edges;
+};
 
-  async loadAlbums() {
-    this.albums = await CameraRoll.getAlbums();
-  }
+export let loadAlbums = async () => {
+  albums = await CameraRoll.getAlbums();
+};
 
-  async createCollections() {
-    // Group photos by day and add them to collections object
-    let collections = {};
-    this.photos.map(crPhoto => {
-      let day = this._toDay(crPhoto.node.timestamp);
-      // collections are identified by a UTC-Day string
-      collections[day] = this.photos.filter(photo => {
-        return this._toDay(photo.node.timestamp).getTime() === day.getTime();
-      });
+export let createCollections = async () => {
+  // Group photos by day and add them to collections object
+  photos.map(crPhoto => {
+    let day = _toDay(crPhoto.node.timestamp);
+    // collections are identified by a UTC-Day string
+    collections[day] = photos.filter(photo => {
+      return _toDay(photo.node.timestamp).getTime() === day.getTime();
     });
+  });
+};
 
-    this.collections = collections;
-  }
+// A helper function to create a JavaScript Date from a timestamp
+let _toDate = timestamp => new Date(timestamp * 1000);
 
-  // A helper function to create a JavaScript Date from a timestamp
-  _toDate = timestamp => new Date(timestamp * 1000);
+// A helper function to create a JavaScript Date from a timestamp, omitting hours, minutes, seconds and miliseconds
+let _toDay = timestamp => {
+  let day = _toDate(timestamp);
+  day.setHours(0, 0, 0, 0);
+  return day;
+};
 
-  // A helper function to create a JavaScript Date from a timestamp, omitting hours, minutes, seconds and miliseconds
-  _toDay = timestamp => {
-    let day = this.toDate(timestamp);
-    day.setHours(0, 0, 0, 0);
-    return day;
-  };
-}
-
-export default PhotoLibrary;
+export default {
+  photos: photos,
+  albums: albums,
+  collections: collections,
+  loadPhotos: loadPhotos,
+  loadAlbums: loadAlbums,
+  createCollections: createCollections,
+};
