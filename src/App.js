ee8df709ce2b0294631a35bf8a0ab44f663e999a
Christoph Pader <chrispad2k@gmail.com>
2020-04-16 12:24:55 +0200

    structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {Home} from './screens/home/Home';
-import {Swipe} from './screens/swipe/Swipe';
+// import {Swipe} from './screens/swipe/Swipe';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 Icon.loadFont();
