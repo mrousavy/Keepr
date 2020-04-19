2f4f7543338e7eb83ed6cb7628ff9197bd9a23a1
Christoph Pader <chrispad2k@gmail.com>
2020-04-19 17:56:11 +0200

    changed HomeModel from .tsx to .ts and added dominantColor

---
+++ src/models/HomeModel.ts
@@ -0,0 +1,41 @@
+import CameraRoll, { PhotoIdentifier, PhotoIdentifiersPage, Album } from '@react-native-community/cameraroll';
+import _ from 'lodash';
+import {RGB, hexToRgb} from '../utils/Colors';
+import { getColorFromURL } from 'rn-dominant-color';
+
+export type Collection = {
+  name: string,
+  dominantColor: RGB,
+  photos: PhotoIdentifier[],
+}
+
+export async function loadPhotos(count = 100) {
+  return await CameraRoll.getPhotos({first: count});
+}
+
+export async function loadAlbums() : Promise<Album[]> {
+  return await CameraRoll.getAlbums({});
+}
+
+export async function createCollections(photos: PhotoIdentifiersPage) : Promise<Collection[]> {
+  // Group photos by day and add them to collections object
+  let groupedCollections = _.toPlainObject(_.groupBy(photos.edges, photo => _toDay(photo.node.timestamp)));
+  let collections: Collection[] = [];
+
+  Object.keys(groupedCollections).map(async collectionName => {
+    let photos = groupedCollections[collectionName] as PhotoIdentifier[];
+    let {primary} = await getColorFromURL(photos[0].node.image.uri);
+    collections.push({name: collectionName, dominantColor: hexToRgb(primary), photos: photos})
+  });
+  return collections;
+}
+
+// A helper function to create a JavaScript Date from a timestamp
+let _toDate = (timestamp: number) => new Date(timestamp * 1000);
+
+// A helper function to create a JavaScript Date from a timestamp, omitting hours, minutes, seconds and miliseconds
+let _toDay = (timestamp: number) => {
+  let day = _toDate(timestamp);
+  day.setHours(0, 0, 0, 0);
+  return day;
+};
